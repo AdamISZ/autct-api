b421683ab5e8b361d6de4023fa6fafc5da5494a0
Adam Gibson <ekaggata@gmail.com>
2024-07-31 10:20:59 -0600

    correct instructions re: proof and reformat

--- src/example.py
+++ src/example.py
@@ -29,36 +29,48 @@ async def run_all_requests_example():
     # parameters for "prove":
     # this is just an example; the private key 040404... stored in a file
     config.privkey_file_str = "privkey-four"
-    # whereever you want to store the proof; note that relative paths are
-    # for the server.
-    config.proof_file_str = "test-proof-file"
+
     proving_request = config.config_to_proof_req()
-    proving_result = await rpc_request(asdict(config), "RPCProver.prove", proving_request)
+    proving_result = await rpc_request(asdict(config), "RPCProver.prove",
+                                       proving_request)
     if proving_result["accepted"] == 0:
         print("Proving request successful!")
     else:
         if proving_result["accepted"] not in RPC_PROOF_ERRORCODES:
-            print("Unrecognized error code from server!: {}".format(proving_result["accepted"]))
+            print("Unrecognized error code from server!: {}".format(
+                proving_result["accepted"]))
         else:
-            print("Proving failed due to error: \n{}".format(RPC_PROOF_ERRORCODES[proving_result["accepted"]]))
+            print("Proving failed due to error: \n{}".format(
+                RPC_PROOF_ERRORCODES[proving_result["accepted"]]))
 
+    # The proof is now available in base64 encoding in the "proof" entry
+    # in the json object that was returned from the server. (Note that it
+    # is not automatically stored in the file `config.proof_file_str` ;
+    # that is only done in the CLI client. If you need to store it, store it yourself.
     print("\nNow we try verifying the proof that we just created:\n")
-    # for the verification, we pass the proof in base64 in a field in the request;
-    # we don't take it directly from a file:
     verifying_req = config.config_to_verify_req(proving_result["proof"])
-    verifying_result = await rpc_request(asdict(config), "RPCProofVerifier.verify", verifying_req)
+    verifying_result = await rpc_request(asdict(config), "RPCProofVerifier.verify",
+                                         verifying_req)
     if verifying_result["accepted"] not in RPC_VERIFY_ERRORCODES:
-        print("Unrecognized error code from server!: {}".format(verifying_result["accepted"]))
+        print("Unrecognized error code from server!: {}".format(
+            verifying_result["accepted"]))
     else:    
-        print("Server response: \n{}".format(RPC_VERIFY_ERRORCODES[verifying_result["accepted"]]))
+        print("Server response: \n{}".format(RPC_VERIFY_ERRORCODES[
+            verifying_result["accepted"]]))
 
-    # lastly we create a new key and taproot address; specify a file location for the private key:
+    # lastly we create a new key and taproot address;
+    # specify a file location for the private key:
     config.privkey_file_str = "new-privkey"
     createkeys_request = config.config_to_createkeys_request()
-    createkeys_result = await rpc_request(asdict(config), "RPCCreateKeys.createkeys", createkeys_request)
-    print("Server response: \n{}".format(RPC_CREATEKEYS_ERRORCODES[createkeys_result["accepted"]]))
+    createkeys_result = await rpc_request(asdict(config),
+                        "RPCCreateKeys.createkeys", createkeys_request)
+    print("Server response: \n{}".format(
+        RPC_CREATEKEYS_ERRORCODES[createkeys_result["accepted"]]))
     if createkeys_result["accepted"] == 0:
-        print("The server generated this address: {}, and stored the corresponding private key in {}".format(createkeys_result["address"], createkeys_result["privkey_file_loc"]))
+        print("The server generated this address: {}, "
+              "and stored the corresponding private key in {}".format(
+                  createkeys_result["address"],
+                  createkeys_result["privkey_file_loc"]))
 
 # Just run the sample requests if this file is executed:
 asyncio.run(run_all_requests_example())
